229b349a248d1d04787e65da7106cda85175437f
Eric Kennedy <ekeric13@gmail.com>
2015-04-10 23:03:57 -0700

    Add piano store

---
+++ client/app/stores/PianoStore.js
@@ -0,0 +1,35 @@
+var Reflux = require("reflux");
+var PianoActions = require("../actions/PianoActions");
+
+var PianoStore = Reflux.createStore({
+
+  listenables: PianoActions,
+
+  _pianos: [],
+
+  getAll: function() {
+    return this._pianos;
+  },
+
+  getAllPianos: function() {
+    var pianos = [{"id": 1, "octave": "1"}, {"id":2, "octave":"2"}];
+    this._pianos = pianos;
+    console.log("in piano store",this._pianos);
+    this.trigger();
+    // NORMALLY WOULD HAVE AJAX
+    // $.ajax({
+    //   type: 'GET',
+    //   url: 'url'
+    // })
+    // .done(function (pianos) {
+    //   // broadcast that _pianos has changed
+    //   this.trigger();
+    // }.bind(this))
+    // .fail(function(error) {
+    //   console.error(error);
+    // });
+  }
+
+});
+
+module.exports = PianoStore;
