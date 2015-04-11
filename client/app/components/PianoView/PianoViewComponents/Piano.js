e2dd09423e26bd4ce6d75759d9e9330388a8ac82
Eric Kennedy <ekeric13@gmail.com>
2015-04-10 23:17:56 -0700

    Allow user to play piano notes

--- client/app/components/PianoView/PianoViewComponents/Piano.js
+++ client/app/components/PianoView/PianoViewComponents/Piano.js
@@ -1,6 +1,21 @@
 var React = require("react");
 var cx = React.addons.classSet;
 
+var noteCounter = {
+  c : 0,
+  cs: 0,
+  d : 0,
+  ds: 0,
+  e : 0,
+  f : 0,
+  fs: 0,
+  g : 0,
+  gs: 0,
+  a : 0,
+  as: 0,
+  b : 0
+};
+
 var Piano = React.createClass({
 
   displayName: 'Piano',
@@ -21,9 +36,26 @@ var Piano = React.createClass({
     });
   },
 
+  noteClick: function(el) {
+    var note = el.dataset.note;
+    noteCounter[note]++
+    var currentNoteCount = noteCounter[note];
+    $(".mid-"+note+"-played").remove();
+    var htmlNote = note.replace("s", "#");
+    $(el).append("<div class='played mid-"+note+"-played'>"+htmlNote+" "+currentNoteCount+"</div>");
+    if (note.substring(1) === "s") {
+      $(".mid-"+note+"-played").addClass("sharp");
+    }
+  },
+
   componentDidMount: function() {
     console.log("props",this.props, this.props._id, this.props.octave);
     this.grabPiano();
+    var self = this;
+    $(".scale li").on("click", function(){
+      var note = this.dataset.note;
+      self.noteClick(this);
+    });
   },
 
 
